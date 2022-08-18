6181b2da0cbd09790fc561a60d66feee7ab9e9b2
Sean Woodward <swoodward@intertech.com>
2022-08-18 16:03:21 +0000

    Additional Logging

--- Api/HydrateClaims.cs
+++ Api/HydrateClaims.cs
@@ -18,7 +18,7 @@ namespace Api
             ILogger log)
         {
             log.LogInformation("HydrateClaims Called");
-            
+
             // Check HTTP basic authorization
             if (!Authorize(req, log))
             {
@@ -64,6 +64,8 @@ namespace Api
 
             // Read the authorization header
             var auth = req.Headers["Authorization"].ToString();
+
+            log.LogInformation($"auth: {auth}");
 
             // Ensure the type of the authorization header id `Basic`
             if (!auth.StartsWith("Basic "))
